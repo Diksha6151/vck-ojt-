bd7f0b1aa31ea17dfcb63cbe5dc3827ec491484a
Diksha6151 <chougalediksha2004@gmail.com>
2025-06-12 13:24:23 +0530

    second commit

--- src/components/Chatbot/ChatbotComponents.jsx
+++ src/components/Chatbot/ChatbotComponents.jsx
@@ -173,7 +173,7 @@ const ChatbotComponent = () => {
               {messages.map((message, index) => (
                 <div key={index} className={`message-row ${message.sender}`}>
                   {message.sender === "bot" && (
-                    <div className="avatar bot-avatar">:robot_face:</div>
+                    <div className="avatar bot-avatar">🤖</div>
                   )}
                   <p className={`${message.sender}-message`}>{message.text}</p>
                   {message.sender === "user" && (
