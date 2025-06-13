ffc025159cbec18668deed8731144dbf4aa9144a
Diksha6151 <chougalediksha2004@gmail.com>
2025-06-13 13:26:57 +0530

    fifth commit

--- src/pages/AdmissionsPage.jsx
+++ src/pages/AdmissionsPage.jsx
@@ -8,7 +8,7 @@ const Admissions = () => {
   return (
     
     <div className="admissions-container">
-      <Headers/>
+      
       <h1 className="main-heading">Admissions at Vivekanand College</h1>
       <p>Welcome to the Admissions page of Vivekanand College! We are delighted you're considering joining our vibrant academic community. Our admission process is designed to be straightforward and accessible.</p>
       <p>Please review the eligibility criteria and key dates below for the upcoming academic year. For any queries, feel free to contact our admissions office.</p>
@@ -58,7 +58,7 @@ const Admissions = () => {
         For detailed information on postgraduate program admissions, specific program syllabi, and fee structures, please visit our
          <Link to="/courses">Courses page</Link> or <Link to="/contact">Contact us</Link> directly.
       </p>
-      <Footer/>
+      
     </div>
   );
 };
