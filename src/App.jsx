5f2ed8cb36810d1b1afad4672194b9f1f6ac2cb9
Diksha6151 <chougalediksha2004@gmail.com>
2025-06-13 12:26:23 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Diksha Pandurang Chougale"
-          studentPhotoUrl="\public\myimage.jpg" // Path to their photo
+          studentPhotoUrl="\myimage.jpg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
